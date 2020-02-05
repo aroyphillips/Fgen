c2dd74e50867a90cfd81f53154df06f79f363c73
aroyphillips <aroyphillips@gmail.com>
2020-02-05 16:21:19 -0600

    updates 2.5.2020

--- fgen_teensy/prototyp.1/prototyp.1.ino
+++ fgen_teensy/prototyp.1/prototyp.1.ino
@@ -1,10 +1,12 @@
-#include <SPI.h>  //SPI library for Arduino
+#include <SPI.h>
+
+
 #define AOut A22
 #define MAXAOUT 5
 float VAL2DAC = 4096/40;
 char ramp_dlm = '^'; // followed by these numbers: is on, distancestart
 
-// Incoming Bit Stream should look like this: '<s1,2,10p0,2,150,10t0,2,150,10,20,10g0,2,120,100,10r0,3,60,10^0,3,150,100>'
+// Incoming Bit Stream should look like this: '<s1,2,10p0,2,150,10!t0,2,150,10,20,10!g0,2,120,100,10!r0,3,60,10!^0,3,150,100!>'
 
 const byte numChars = 32;
 char DC[numChars];
@@ -16,9 +18,16 @@ char tri[numChars];
 
 boolean newData = false;
 
+boolean isDC = false;
+boolean isPulse = false;
+boolean isTrain = false;
+boolean isGaus = false;
+boolean isRamp = false;
+boolean isTri = false;
+
 void setup() {
     Serial.begin(9600);
-    Serial.println("<Arduino is ready>");
+    Serial.println("<Teensy is ready>");
 }
 
 void loop() {
@@ -31,6 +40,7 @@ void recvWithStartEndMarkers() {
     static byte ndx = 0;
     char startMarker = '<';
     char endMarker = '>';
+    char phraseEnd = '!';
     char rc;
  
     while (Serial.available() > 0 && newData == false) {
@@ -42,19 +52,107 @@ void recvWithStartEndMarkers() {
         }
         
         else if (recvInProgress == true) {
+          
+         
             if (rc != endMarker) {
-                switch(n) {
+                switch(rc) {
                   case 's':
-                    
+                    // Receiving parameters for DC step
+                    clearBools();
+                    ndx = 0;
+                    isDC = true;
+
+                  case 'p':
+                    // Receiving parameters for Pulse 
+                    clearBools();
+                    ndx = 0;
+                    isPulse = true;
+
+                  case 't':
+                    // Receiving parameters for Pulse Train
+                    clearBools();
+                    ndx = 0;
+                    isTrain = true;
         
+                  case 'g':
+                    // Receiving parameters for Gaussian
+                    clearBools();
+                    ndx = 0;
+                    isGaus = true;
+
+                  case 'r':
+                    // Receiving parameters for Ramp
+                    clearBools();
+                    ndx = 0;
+                    isRamp = true;
+
+                  case '^':
+                    // Receiving parameters for triangle pulse
+                    clearBools();
+                    ndx = 0;
+                    isTri = true;
+
+                  default:
+                   // Receiving parameters based on flag
+                   if (isDC){
+                    if (rc == phraseEnd){
+                      DC[ndx] = '/0'; // end string
+                    }
+                    else{
+                      DC[ndx] = rc;
+                    }
+                   }
+                   else if (isPulse){
+                    if (rc == phraseEnd){
+                      pulse[ndx] = '/0'; // end string
+                    }
+                    else{
+                    pulse[ndx] =rc;
+                    }
+                   }
+                   else if (isTrain){
+                    if (rc == phraseEnd){
+                      train[ndx] = '/0'; // end string
+                    }
+                    else{
+                      train[ndx] = rc;
+                    }
+                   }
+                   else if (isGaus){
+                    if (rc == phraseEnd){
+                      gaus[ndx] = '/0'; // end string
+                    }
+                    else{
+                      gaus[ndx] = rc;
+                    }
+                   }
+                   else if (isRamp){
+                    if (rc == phraseEnd){
+                      ramp[ndx] = '/0'; // end string
+                    }
+                    else{
+                      ramp[ndx] = rc;
+                    }
+                   }
+                   else if (isTri){
+                    if (rc == phraseEnd){
+                      tri[ndx] = '/0'; // end string
+                    }
+                    else{
+                      tri[ndx] = rc;
+                    }
+                   }
+                   else{
+                    noFlags();                                      
+                   }
+                   ndx++;
                 }
-                ndx++;
+                
                 if (ndx >= numChars) {
                     ndx = numChars - 1;
                 }
             }
             else {
-                receivedChars[ndx] = '\0'; // terminate the string
                 recvInProgress = false;
                 ndx = 0;
                 newData = true;
@@ -63,9 +161,30 @@ void recvWithStartEndMarkers() {
     }
 }
 
-void runNewData() {
+void buildNewData() {
     if (newData == true) {
         newData = false;
+    }  
+    if (DC[0] == '1'){
+      outputVolts(DC);
     }
-    
+}
+
+
+void outputVolts(char DC[]){
+   
+}  
+
+
+void clearBools() {
+  boolean isDC = false;
+  boolean isPulse = false;
+  boolean isTrain = false;
+  boolean isGaus = false;
+  boolean isRamp = false;
+  boolean isTri = false;
+}
+
+void noFlags() {
+  Serial.print("No shape flags read");
 }
