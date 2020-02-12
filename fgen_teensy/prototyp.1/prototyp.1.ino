329fb1a4eb3e0e2de4382d1b0bac4616b9ca46cf
aroyphillips <aroyphillips@gmail.com>
2020-02-12 15:37:48 -0600

    updates 2.12.2020, working step read, fixed char* to boolean issue

--- fgen_teensy/prototyp.1/prototyp.1.ino
+++ fgen_teensy/prototyp.1/prototyp.1.ino
@@ -51,7 +51,6 @@ void recvWithStartEndMarkers() {
     char endMarker = '>';
     char phraseEnd = '!';
     char rc;
- 
     while (Serial.available() > 0 && newData == false) {
         rc = Serial.read();
 
@@ -61,102 +60,113 @@ void recvWithStartEndMarkers() {
         }
         
         else if (recvInProgress == true) {
-          
+            
             // Read parameters for different output shapes
             if (rc != endMarker) {
                 switch(rc) {
                   case 's':
                     // Receiving parameters for DC step
+                    Serial.println("reading step");
                     clearBools();
                     ndx = 0;
                     isDC = true;
-
+                    break;
                   case 'p':
                     // Receiving parameters for Pulse 
                     clearBools();
+                    Serial.println("reading pulse");
                     ndx = 0;
                     isPulse = true;
-
+                    break;
                   case 't':
                     // Receiving parameters for Pulse Train
                     clearBools();
                     ndx = 0;
                     isTrain = true;
-        
+                    break;      
                   case 'g':
                     // Receiving parameters for Gaussian
                     clearBools();
                     ndx = 0;
                     isGaus = true;
-
+                    break;
                   case 'r':
                     // Receiving parameters for Ramp
                     clearBools();
                     ndx = 0;
                     isRamp = true;
-
+                    break;
                   case '^':
                     // Receiving parameters for triangle pulse
                     clearBools();
                     ndx = 0;
                     isTri = true;
-
+                    break;
                   default:
                    // Receiving parameters based on flag
                    if (isDC){
                     if (rc == phraseEnd){
+                      Serial.println("end step");
                       DC[ndx] = '/0'; // end string
+                      Serial.println(DC);
                     }
-                    else{
+                    else if (rc!='s'){
                       DC[ndx] = rc;
+                      ndx++;
                     }
                    }
                    else if (isPulse){
                     if (rc == phraseEnd){
+                      Serial.println("end pulse");
                       pulse[ndx] = '/0'; // end string
+                      Serial.println(pulse);
                     }
-                    else{
+                    else if (rc!='p'){
                     pulse[ndx] =rc;
+                    ndx++;
                     }
                    }
                    else if (isTrain){
                     if (rc == phraseEnd){
                       train[ndx] = '/0'; // end string
                     }
-                    else{
+                    else if (rc!='t'){
                       train[ndx] = rc;
+                      ndx++;
                     }
                    }
                    else if (isGaus){
                     if (rc == phraseEnd){
                       gaus[ndx] = '/0'; // end string
                     }
-                    else{
+                    else if (rc!='g'){
                       gaus[ndx] = rc;
+                      ndx++;
                     }
                    }
                    else if (isRamp){
                     if (rc == phraseEnd){
                       ramp[ndx] = '/0'; // end string
                     }
-                    else{
+                    else if (rc!= 'r'){
                       ramp[ndx] = rc;
+                      ndx++;
                     }
                    }
                    else if (isTri){
                     if (rc == phraseEnd){
                       tri[ndx] = '/0'; // end string
                     }
-                    else{
+                    else if (rc!='^'){
                       tri[ndx] = rc;
+                      ndx++;
                     }
                    }
                    else{
                     noFlags();                                      
                    }
-                   ndx++;
+                   break;
                 }
-                
                 if (ndx >= numChars) {
                     ndx = numChars - 1;
                 }
@@ -172,30 +182,33 @@ void recvWithStartEndMarkers() {
 
 void buildNewData() {
     if (newData == true) {
-        newData = false;
-    }  
-    if (DC[0] == '1'){
-      parseDCData(DC);
-      outputVolts(isDCactive, DCamp, DCdelay);
-    }
+        Serial.println("Output");
+        parseDCData(DC);
+        outputVolts(isDCactive, DCamp, DCdelay);
+        newData = false;    
+      }
+       
 }
 
 
-void outputVolts(boolean activeDC, float ampDC, float delayDC){
-   if (activeDC){
+void outputVolts(boolean DCactiv, float ampDC, float delayDC){
+    if (DCactiv){
     delay(delayDC);
-    analogWrite(AOut, ampDC*VAL2DAC); 
-   }
+    analogWrite(AOut, ampDC*VAL2DAC);
+    }
+    else{
+      analogWrite(AOut,0); 
+    }
 }  
 
 
 void clearBools() {
-  boolean isDC = false;
-  boolean isPulse = false;
-  boolean isTrain = false;
-  boolean isGaus = false;
-  boolean isRamp = false;
-  boolean isTri = false;
+  isDC = false;
+  isPulse = false;
+  isTrain = false;
+  isGaus = false;
+  isRamp = false;
+  isTri = false;
 }
 
 void noFlags() {
@@ -207,15 +220,22 @@ void parseDCData(char DCstr[]) {
     // split the data into its parts
     
   char * strtokIndx; // this is used by strtok() as an index
-  
-  
+
   strtokIndx = strtok(DCstr, ","); // this continues where the previous call left off
-  isDCactive =(strtokIndx=='1');     // convert this part to an integer
-  
+  isDCactive =(*strtokIndx=='1');     // converts char* to a vboolean
+  Serial.println(strtokIndx);
+  types(strtokIndx);
+  Serial.println(isDCactive);
   strtokIndx = strtok(NULL, ",");
   DCamp = atof(strtokIndx);     // convert this part to a integer
-
+  Serial.println(DCamp);
   strtokIndx = strtok(NULL, ",");
   DCdelay = atof(strtokIndx);     // convert this part to a integer
 
 }
+
+// to get variable type
+void types(String a){Serial.println("it's a String");}
+void types(int a)   {Serial.println("it's an int");}
+void types(char* a) {Serial.println("it's a char*");}
+void types(float a) {Serial.println("it's a float");} 
