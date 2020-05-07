5b4d71e8ce20d3a6e8227da7692f8c1a1a367d38
Roy Phillips <aroyphillips@gmail.com>
2020-05-07 18:01:57 -0500

    Tested, updated two train GUIs

--- Fgen_Matlab/manualSerial.m
+++ Fgen_Matlab/manualSerial.m
@@ -9,14 +9,15 @@ fopen(SerialID);
 
 N = 1000;
 tests = NaN(2,N);
-tic
+t0 = tic;
 for ii = 1:N
 flushinput(SerialID);
-this = fread(SerialID,1, 'uint8')==255
-tests(ii) = this;
-end
-toc
+this = fread(SerialID,1, 'uint8')==255;
+dt = toc(t0);
+fprintf("Time: %3.5f, Val: %g\n", dt, this);
+tests(:,ii) = [this;dt];
 
+end
 
 
 %teststr = '<^1,2,100,80,30!>';
